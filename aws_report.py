f767f19c90e4bf8f0e40463daaf2143051842ec2
Cody Herriges <cody@herriges.org>
2014-12-16 14:34:51 -0800

    Add inline documentation to aws_report.py
      Commit will add inline documenation to portions of script to help
      identify what various piece of code is actually doing or trying to
      accomplish.

--- aws_report.py
+++ aws_report.py
@@ -11,6 +11,8 @@ import sendgrid
 
 def main(argv=sys.argv):
 
+  # Setting default script option parameters before reading command line
+  # options give.  Didn't find a better documented way.
   email = False
 
   try:
@@ -33,6 +35,7 @@ def main(argv=sys.argv):
     elif opt in ("-m", "--mail"):
         email = True
 
+  # So much for time.
   current_time = datetime.datetime.now(pytz.utc)
   yesterday_time = current_time - datetime.timedelta(days=1)
   current_formatted = str(current_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + current_time.strftime("%z"))
@@ -41,8 +44,18 @@ def main(argv=sys.argv):
   yesterday_index = str(yesterday_time.strftime("%Y.%m.%d"))
   yesterday_email = str(yesterday_time.strftime("%Y-%m-%d"))
 
+  # To help infer the owner of an instance we check the last two days of
+  # logstash indexes for cloudtrail tracked AWS API calls.
   user_search = "http://elasticsearch.ops.puppetlabs.net:9200/logstash-" + current_index + ",logstash-" + yesterday_index + "/_search"
+
+  # We'll be searching through our aws-audit index for violators
   route_search = "http://elasticsearch.ops.puppetlabs.net:9200/aws-audits/_search"
+
+  # A bunch of mostly static data that makes up an ElasticSearch search query.
+  # You might notice that size is set to 0.  This is because we query the index
+  # twice, once to get the number of matches and then again to retrieve all the
+  # matches.  This is to avoid having to deal with paging since we are not
+  # querying a terribly large index.
   hits_request = {
     "query": {
       "filtered": {
@@ -84,12 +97,19 @@ def main(argv=sys.argv):
     ]
   }
 
+  # Get the number of hits to our query
   hits = requests.get(route_search, data=json.dumps(hits_request)).json()
 
+  # Modify size key of hits_request dictionary to match the number of query
+  # hits from previous require.
   hits_request["size"] = hits["hits"]["total"]
 
+  # Get all the hits as json
   data = requests.get(route_search, data=json.dumps(hits_request)).json()
 
+  # The pattern I just explained, you'll see it again but this time for getting
+  # data from logstash indexes of type cloudtrail for API calls that create or
+  # boot stopped instances.
   user_request = {
     "query": {
       "filtered": {
@@ -171,18 +191,31 @@ def main(argv=sys.argv):
 
   user_data = requests.get(user_search, data=json.dumps(user_request)).json()
 
+  # Now we start contructing the body of an email report to send
   grouped = dict()
+
+  # Email header.
   doc = "This report is a list of all instances that will be terminated by the SysOps EC2 audit scripts in the last 24 hours for tag scheme violations come January 12, 2015.  To prevent interruption of work or loss of data you must tag instances to the documented scheme at https://confluence.puppetlabs.com/display/OPS/Cloud+Asset+Management+Standards.\n\n"
 
   users = dict()
   user = str()
 
+  # Reducing the cloudtail data obtained from logstash to a simple lookup table
+  # of instanceId to username mappings.
   for u in user_data['hits']['hits']:
     users[u['_source']['responseElements']['instancesSet']['items'][0]['instanceId']] = u['_source']['userIdentity']['userName']
 
+  # Iterate through all the data obtained from aws_audits and create a key for
+  # each IAM alias, set to an empty set.
   for h in data["hits"]["hits"]:
     grouped[h["_source"]["account"]] = set([])
 
+  # For each unique IAM alias iterate through aws_audits data, obtaining each
+  # instanceId and looking that ID up in the users lookup table we created
+  # earlier from the logstash data to identify possible owner of instance.
+  # Construct a string of instanceId, inferred user, and any tags assigned to the
+  # instance and added that to the set that corresponds to the IAM alias it was
+  # associated with.
   for g in grouped:
     for h in data["hits"]["hits"]:
       if h['_source']['account'] == g:
@@ -193,12 +226,14 @@ def main(argv=sys.argv):
 
         grouped[g].add("ID: " + h['_source']['id'] + " USER: " + user + " TAGS: " + ",".join(h['_source']['tags']))
 
+  # Add a section to our email body for each IAM alias
   for g in grouped:
     doc += (g + " IAM root account: TO BE TERMINATED" + "\n")
     for i in grouped[g]:
       doc += ("  " + i + "\n")
     doc += "\n"
 
+  # Send the email via SendGrid
   if email == True:
     sg = sendgrid.SendGridClient(sg_user, sg_password)
 
